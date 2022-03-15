d1de64d67ab05bc37851543efb88410c5d968150
Daye <poiuylk56@gmail.com>
2022-03-16 02:45:12 +0900

    #4.3 Events part Two

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault(); //브라우저의 기본 동작 실행 방지
   //submit시 자동 새로고침을 방지하고자 사용함
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
