6a79690b244a91f4dd8eb3f767f2bc524ab4de94
Daye <poiuylk56@gmail.com>
2022-03-16 03:10:23 +0900

    #4.4 Getting Username

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault(); //브라우저의 기본 동작 실행 방지
   //submit시 자동 새로고침을 방지하고자 사용함
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username} keep going`; //추천, 백틱(`)
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
