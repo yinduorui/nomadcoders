c71498d8b0ba79b6472af65910c7dc7afddceaa9
Daye <poiuylk56@gmail.com>
2022-04-20 18:46:28 +0900

    Tue.19.Apr(4)

--- javascript/JsChrome_3OnTheBrowser/app.js
+++ javascript/JsChrome_3OnTheBrowser/app.js
@@ -1,11 +1,11 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const className = "acitve";
-  if (h1.className === className) {
+  const clickedClass = "acitve";
+  if (h1.className === clickedClass) {
     h1.className = "";
   } else {
-    h1.className = className;
+    h1.className = clickedClass;
   }
 }
 
