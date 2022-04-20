117de28b5af271859d79b136ca7d3751e0662bc5
Daye <poiuylk56@gmail.com>
2022-04-20 18:54:41 +0900

    Tue.19.Apr(4-2)

--- javascript/JsChrome_3OnTheBrowser/app.js
+++ javascript/JsChrome_3OnTheBrowser/app.js
@@ -2,11 +2,12 @@ const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
   const clickedClass = "acitve";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
 }
+//className: 기존 class 보존 불가
 
 h1.addEventListener("click", handleTitleClick);
