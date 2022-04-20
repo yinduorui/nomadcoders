b2f59d55f2e98e56e2378ec85f0d153bbf76d9dc
Daye <poiuylk56@gmail.com>
2022-04-20 18:12:28 +0900

    Tue.19.Apr(2)

--- javascript/JsChrome_3OnTheBrowser/app.js
+++ javascript/JsChrome_3OnTheBrowser/app.js
@@ -13,6 +13,27 @@ function handleMouseLeave() {
   title.innerText = "Mouse is gone!";
 }
 
+function handleResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleCopy() {
+  alert("copier!");
+}
+
+function handleOffline() {
+  alert("SOS no WIFI!");
+}
+
+function handleOnline() {
+  alert("ALL GOOOOOOD!");
+}
+
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleResize);
+window.addEventListener("copy", handleCopy);
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
