a9cab2fd139f2068537325046f4a3044fa522668
Daye <poiuylk56@gmail.com>
2022-04-21 01:29:15 +0900

    Wed.20.Apr

--- javascript/JsChrome_5CLOCK/js/clock.js
+++ javascript/JsChrome_5CLOCK/js/clock.js
@@ -1,1 +1,14 @@
 const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+/*
+setInterval(getClock, 5000); //5000ms 이후 5000ms 마다 반복 실행
+setTimeout(getClock, 5000); //5000ms 이후 1번만 실행
+*/
