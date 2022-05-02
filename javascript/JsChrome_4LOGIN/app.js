e8911d3b4002d2aaaf12b239db9b06e4ccbda791
Daye <poiuylk56@gmail.com>
2022-05-02 17:02:14 +0900

    Login_recap

--- javascript/JsChrome_4LOGIN/app.js
+++ javascript/JsChrome_4LOGIN/app.js
@@ -3,31 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"; //String 오타는 JS가 디버깅 불가. 변수명은 가능
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event) {
-  event.preventDefault(); //브라우저의 기본 동작 실행 방지
-  //submit시 자동 새로고침을 방지하고자 사용함
+function loginSubmit(event) {
+  event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username); //개발자도구>Application>Local Strorage 확인!
-  //불러오기: localStorage.getItem("keyName")
-  //삭제: localStorage.removeItem("keyName")
-  paintGreetings(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.addEventListener("submit", loginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  paintGreeting(savedUsername);
 }
