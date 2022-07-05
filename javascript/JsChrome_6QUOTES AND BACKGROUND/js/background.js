2d329c0c4d6acd123ff262f47d52e6918b99e389
daye <94037368+yinduorui@users.noreply.github.com>
2022-07-05 11:04:27 +0900

    first-kokoa

--- javascript/JsChrome_6QUOTES AND BACKGROUND/js/background.js
+++
@@ -1,7 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.JPG"];
-
-const choosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImg = document.createElement("img");
-
-bgImg.src = `img/${choosenImage}`;
-document.body.appendChild(bgImg);
